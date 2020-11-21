a29495299367b49b6ab644da9fbeac8b2e44e4fc
zabir1998 <zabirahmed1998@gmail.com>
2020-11-22 03:47:14 +0600

    front end done

---
+++ README.md
@@ -0,0 +1,37 @@
+# Athena-design (A Reactjs design & responsive based project)
+
+### Contributor Name:
+[Md Jidanul Hakim Jitu](https://github.com/jh-jitu)
+
+[Zabir Siddique](https://github.com/zabir1998)
+
+[Md Touhedul Haque](https://github.com/touhedulhaque)
+
+#### Live-Link: https://athena-design-demo.netlify.app/
+
+
+`Back to` [Resume of Haque](https://drive.google.com/file/d/1OPYsxd5woKjk59KSaLfclYRCAxCbx66D/view).
+
+- A Reactjs design & responsive based Project.
+- An practiced project.
+- small team project.
+- focused on css, bootstrap, media-query.
+
+### `Technology used:`
+- React-Js 
+- bootstrap
+- css & html.
+- media query.
+
+Back to [Resume of Haque](https://drive.google.com/file/d/1OPYsxd5woKjk59KSaLfclYRCAxCbx66D/view).
+
+Best Regards
+
+Md Touhedul Haque
+
+Address: Bändlistrasse 25, 8064, Zurich.
+
++41 (0) 789696677
+
+[GitHub](https://github.com/touhedulhaque)    [LinkedIn](https://www.linkedin.com/in/md-touhedul-haque)    [facebook](https://facebook.com/haque.touhedul)    [Twitter](https://twitter.com/HaqueTouhedul)    [Medium](https://haque-touhedul.medium.com/)
+
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { createContext, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
+import './App.css';
+import AddAdmin from './Components/Dashboard/Addadmin/AddAdmin';
+import AddService from './Components/Dashboard/AddService/AddService';
+import Dashboard from './Components/Dashboard/Dashboard/Dashboard';
+import ServiceList from './Components/Dashboard/Dashboard/ServiceList/ServiceList';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
+
+function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+  
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+    <Router>
+    <Switch>
+    <Route exact path="/">
+        <Home></Home>
+    </Route>
+    <Route path="/home">
+        <Home></Home>
+      </Route>
+      <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/addService">
+            <AddService></AddService>
+          </Route>
+          <Route path="/serviceList">
+            <ServiceList></ServiceList>
+          </Route>
+          <Route path="/addAdmin">
+            <AddAdmin></AddAdmin>
+          </Route>
+          
+
+      <PrivateRoute path="/dashboard">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+      </Switch>
+      </Router>
+      </UserContext.Provider>
+  );
+}
+
+export default App;
